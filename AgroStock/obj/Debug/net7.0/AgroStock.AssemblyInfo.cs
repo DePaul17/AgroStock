63ff9f374600d5b7ce67348f2809e3160761a0a7
Mairymhub <Falek.myriam@hotmail.com>
2025-02-12 10:39:45 +0100

    Changement des titres
    Changement des titres en cas de modification pour catégories et sous cat

--- AgroStock/obj/Debug/net7.0/AgroStock.AssemblyInfo.cs
+++ AgroStock/obj/Debug/net7.0/AgroStock.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgroStock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52cfebcef21661fd8d65b41668db9521594ef8b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9e4d4e412be728385a387d4a94de9012cb16c80")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgroStock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgroStock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
